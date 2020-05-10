c37a51475cb0c0d4d3dd4bf6e0d91755cf38760e
Ioannis Sporidis-Foster <dmjohnspor@gmail.com>
2020-05-10 17:40:34 -0400

    Set up DELETE/api/note and fixed minor issues

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 // =============================================================
@@ -21,19 +21,19 @@ const DB_DIR = path.resolve(__dirname, "db");
 const indexPath = path.join(HTML_DIR, "index.html");
 const notesPath = path.join(HTML_DIR, "notes.html");
 const dbPath = path.join(DB_DIR, "db.json");
-// console.log(dbPath);
-
+app.use(express.static("public"));
 
 // HTML routes
 // =============================================================
 app.get("/", (req, res) => { res.sendFile(indexPath) });
 app.get("/notes", (req, res) => { res.sendFile(notesPath) });
 
+
 // API routes
 // =============================================================
 // Reading data from db.json and storing it as a variable:
-const data = fs.readFileSync(dbPath, "utf8");
-const savedNotes = JSON.parse(data);
+let data = fs.readFileSync(dbPath, "utf8");
+let savedNotes = JSON.parse(data);
 
 // Returns all saved notes as json:
 app.get("/api/notes", (req, res) => {
@@ -45,12 +45,22 @@ app.post("/api/notes", (req, res) => {
     const newNote = req.body;
     savedNotes.push(newNote)
     console.log(savedNotes);
-    const newData = JSON.stringify(savedNotes, null, 2);
+    let newData = JSON.stringify(savedNotes, null, 2);
     fs.writeFile(dbPath, newData, (err) => { if (err) { throw err } });
     res.json(newData);
 });
 
-app.delete("/api/notes/:id", (req, res) => { });
+app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    savedNotes = savedNotes.filter((note) => {
+        if (note.id === id) { return false }
+        else { return true }
+    })
+    console.log(savedNotes);
+    // let newData = JSON.stringify(savedNotes, null, 2);
+    // fs.writeFile(dbPath, newData, (err) => { if (err) { throw err } });
+    // res.json(newData);
+});
 
 // Listener
 // =============================================================
